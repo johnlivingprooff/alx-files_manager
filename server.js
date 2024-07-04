4be417e165ee29c5bf182ab6eedf1337cd5ca9b1
johnlivingprooff <johnlivingprooff@gmail.com>
2024-07-04 08:35:34 +0200

    2. First API

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Load routes
+app.use('/', routes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+export default app;
